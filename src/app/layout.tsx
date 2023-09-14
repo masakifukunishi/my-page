246640ca35899835c8061b6386f33dbd29bc3f47
MasakiFukunishi <fmsknn@gmail.com>
2023-09-14 12:37:16 -0700

    Add coloer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} text-gray-50 bg-gray-900`}>
+      <body className={`${inter.className} text-gray-50 bg-background`}>
         <Header />
         {children}
         <Footer />
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,15 +1,46 @@
-import Link from "next/link";
+"use client";
+import { useState } from "react";
 
 import HeaderLink from "./HeaderLink";
+import MobileNavigationLink from "./MobileNavigationLink";
+
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openMenu = () => setIsOpen(true);
+  const closeMenu = () => setIsOpen(false);
+
   return (
-    <header className="fixed top-0 left-0 w-full bg-gray-800 h-14 px-8 flex items-center ">
+    <header className="fixed top-0 left-0 w-full h-16 px-8 flex items-center bg-header bg-opacity-95">
       <h1 className="text-2xl font-semibold">Masaki Fukunishi</h1>
       <div className="ml-auto">
-        <HeaderLink href="#about" menu="About" />
-        <HeaderLink href="#career" menu="Career" />
-        <HeaderLink href="#apps" menu="Apps" />
+        <div className="hidden md:flex">
+          <HeaderLink href="#about" menu="About" />
+          <HeaderLink href="#career" menu="Career" />
+          <HeaderLink href="#apps" menu="Apps" />
+        </div>
+        <div className="md:hidden flex items-center">
+          <button onClick={openMenu}>
+            <div className="w-6 h-0.5 my-1 bg-gray-50"></div>
+            <div className="w-6 h-0.5 my-1 bg-gray-50"></div>
+            <div className="w-6 h-0.5 my-1 bg-gray-50"></div>
+          </button>
+        </div>
       </div>
+      {isOpen && (
+        <div className="md:hidden fixed top-0 right-0 w-56 h-full bg-gray-800 flex items-center flex-col px-8 pt-5 bg-header bg-opacity-95">
+          <button onClick={closeMenu} className="ml-auto">
+            <svg xmlns="http://www.w3.org/2000/svg" className="w-7 h-7" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+            </svg>
+          </button>
+          <div className="flex flex-col items-center">
+            <MobileNavigationLink href="#about" menu="About" handleClick={closeMenu} />
+            <MobileNavigationLink href="#career" menu="Career" handleClick={closeMenu} />
+            <MobileNavigationLink href="#apps" menu="Apps" handleClick={closeMenu} />
+          </div>
+        </div>
+      )}
     </header>
   );
 };
