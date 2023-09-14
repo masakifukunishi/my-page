7da375c0a4cd80287c00968efd2b2beab9163cde
MasakiFukunishi <fmsknn@gmail.com>
2023-09-14 14:31:42 -0700

    Add icons

---
+++ src/components/header/icons/Hamburger.tsx
@@ -0,0 +1,10 @@
+const Hamburger = () => {
+  return (
+    <>
+      <div className="w-6 h-0.5 my-1 bg-gray-50"></div>
+      <div className="w-6 h-0.5 my-1 bg-gray-50"></div>
+      <div className="w-6 h-0.5 my-1 bg-gray-50"></div>
+    </>
+  );
+};
+export default Hamburger;
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 
 import HeaderLink from "./HeaderLink";
 import MobileNavigationLink from "./MobileNavigationLink";
+import Cross from "./icons/Cross";
+import Hamburger from "./icons/Hamburger";
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -21,18 +23,14 @@ const Header = () => {
         </div>
         <div className="md:hidden flex items-center">
           <button onClick={openMenu}>
-            <div className="w-6 h-0.5 my-1 bg-gray-50"></div>
-            <div className="w-6 h-0.5 my-1 bg-gray-50"></div>
-            <div className="w-6 h-0.5 my-1 bg-gray-50"></div>
+            <Hamburger />
           </button>
         </div>
       </div>
       {isOpen && (
         <div className="md:hidden fixed top-0 right-0 w-56 h-full bg-gray-800 flex items-center flex-col px-8 pt-5 bg-header bg-opacity-95">
           <button onClick={closeMenu} className="ml-auto">
-            <svg xmlns="http://www.w3.org/2000/svg" className="w-7 h-7" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
-            </svg>
+            <Cross />
           </button>
           <div className="flex flex-col items-center">
             <MobileNavigationLink href="#about" menu="About" handleClick={closeMenu} />
