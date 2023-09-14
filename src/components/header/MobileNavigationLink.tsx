525e73e37e2899009c04c39e98e671634cf68daf
MasakiFukunishi <fmsknn@gmail.com>
2023-09-14 16:11:31 -0700

    Change header lauout

--- src/components/header/MobileNavigationLink.tsx
+++ src/components/header/MobileNavigationLink.tsx
@@ -8,7 +8,7 @@ interface MobileNavigationLinkProps {
 
 const MobileNavigationLink: React.FC<MobileNavigationLinkProps> = ({ href, menu, handleClick }) => {
   return (
-    <Link href={href} className="text-2xl font-semibold mt-3" onClick={handleClick}>
+    <Link href={href} className="text-2xl font-semibold mt-5" onClick={handleClick}>
       {menu}
     </Link>
   );
