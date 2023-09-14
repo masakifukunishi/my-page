755f419dd50f01b74939d2494cbf0127bfe23176
MasakiFukunishi <fmsknn@gmail.com>
2023-09-14 14:06:41 -0700

    Change footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,5 +1,5 @@
 const Footer = () => {
-  return <div className="text-center text-sm text-gray-200 py-3">&copy; Masaki Fukunishi {new Date().getFullYear()}</div>;
+  return <div className="text-center text-sm text-gray-200 pt-1 pb-3">&copy; Masaki Fukunishi {new Date().getFullYear()}</div>;
 };
 
 export default Footer;
