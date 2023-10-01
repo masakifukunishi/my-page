a13132ce2e6a0c1f34eb2cfad3a4884a6adbf828
MasakiFukunishi <fmsknn@gmail.com>
2023-09-30 23:05:55 -0700

    Add link to crypto chart demo

--- src/components/apps/index.tsx
+++ src/components/apps/index.tsx
@@ -10,14 +10,17 @@ const Apps = () => {
         appName="Crypto Chart"
         imagePath="/apps/crypto-chart.png"
         description={`Crypto Chart is a full-stack MERN app. \n lets users view candlestick charts of cryptocurrencies for selected asset pairs and periods 📈`}
-        links={[{ service: "GitHub", href: "https://github.com/masakifukunishi/crypto-chart" }]}
+        links={[
+          { service: "Demo", href: "https://crypto-chart-1r7g.onrender.com" },
+          { service: "GitHub", href: "https://github.com/masakifukunishi/crypto-chart" },
+        ]}
       />
       <App
         appName="PWA List"
         imagePath="/apps/pwa-list.png"
         description={`Web app that lets developers post PWAs and users view them. \n More than 150 PWAs have been submitted by developers 🙌`}
         links={[
-          { service: "Live", href: "https://www.pwalist.app" },
+          { service: "App", href: "https://www.pwalist.app" },
           { service: "GitHub", href: "https://github.com/masakifukunishi/pwa-list" },
         ]}
       />
