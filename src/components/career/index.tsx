b1d34a2cfa75372cd78ec64d0e19b6ca5ef893ec
MasakiFukunishi <fmsknn@gmail.com>
2023-10-10 20:39:41 -0700

    Fix job experience

--- src/components/career/index.tsx
+++ src/components/career/index.tsx
@@ -14,7 +14,7 @@ const Career = () => {
           subTitles={["Web system to support decarbonization and customer management", "Web scraping to streamline operations"]}
           responsibilities={[
             [
-              "Improved dashboard response speed by modifying backend logic, optimizing SQL queries, and separating APIs. This has enhanced the user experience as all the charts in the dashboard are displayed about 2500 milliseconds faster",
+              "Improved dashboard response speed by modifying backend logic, optimizing SQL queries, and separating APIs. This has enhanced the user experience as all the charts in the dashboard are displayed about 2,500 milliseconds faster",
               "Implemented a feature to calculate and view greenhouse gas emissions for each of the company's activities using Laravel and Vue.js. This feature has been patented",
             ],
             [
