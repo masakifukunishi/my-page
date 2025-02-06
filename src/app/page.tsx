3e97cc02733b4d906d340c53f572f55cd748fcbc
MasakiFukunishi <fmsknn@gmail.com>
2025-02-07 01:12:42 +0900

    fix padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Projects from "../components/projects";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-10 md:p-8">
+    <main className="flex min-h-screen flex-col items-center p-6 md:p-8">
       <div className="w-full max-w-4xl">
         <AboutMe />
         <Projects />
