88115123566ec61af3f6120df1f94ff421e676bf
MasakiFukunishi <fmsknn@gmail.com>
2026-06-14 23:45:56 +0900

    fix description

--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -17,13 +17,13 @@ const Projects = () => {
       />
       <Project
         appName="Blog service with DDD (Sample Project)"
-        description={`Demonstrating the implementation of a blog service using Domain-Driven Design (DDD) and Layered Architecture principles. \n Docker, TypeScript, Node.js, Express, Prisma, SQLite, Vitest, ESLint, Prettier, GitHub Actions (CI)`}
+        description={`A sample blog service demonstrating Domain-Driven Design (DDD) and layered architecture. \n Skills : Docker, TypeScript, Node.js, Express, Prisma, SQLite, Vitest, ESLint, Prettier, GitHub Actions (CI)`}
         links={[{ service: "GitHub", href: "https://github.com/masakifukunishi/ddd-blog-api" }]}
       />
       <Project
         appName="NFT Craft"
         imagePath="/projects/nft-craft.png"
-        description={`Web app that allows users to mint NFTs (ERC-721 tokens) and view them.`}
+        description={`A web app that allows users to mint and view NFTs (ERC-721 tokens).`}
         links={[
           { service: "App (Testnet)", href: "https://nft-craft-ten.vercel.app" },
           { service: "GitHub", href: "https://github.com/masakifukunishi/nft-craft" },
@@ -32,13 +32,13 @@ const Projects = () => {
       <Project
         appName="Crypto Chart"
         imagePath="/projects/crypto-chart.png"
-        description={`Full-stack MERN app. \n Lets users view candlestick charts of cryptocurrencies for selected asset pairs and periods 📈`}
+        description={`A full-stack MERN app that lets users view candlestick charts for selected cryptocurrency pairs and time periods. 📈`}
         links={[{ service: "GitHub", href: "https://github.com/masakifukunishi/crypto-chart" }]}
       />
       <Project
         appName="PWA List"
         imagePath="/projects/pwa-list.png"
-        description={`Web app that lets developers post PWA and users view them. \n More than 300 PWA have been submitted by developers!`}
+        description={`A web app that lets developers submit PWAs and users discover them. \n More than 300 PWAs have been submitted by developers!`}
         links={[{ service: "App", href: "https://www.pwalist.app", isStrikethrough: true }]}
       />
     </div>
