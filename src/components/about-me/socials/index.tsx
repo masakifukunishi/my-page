a89710d0f18bcc4c92774bcce6b9e38b6dd6436d
MasakiFukunishi <fmsknn@gmail.com>
2026-06-14 23:47:08 +0900

    change email

--- src/components/about-me/socials/index.tsx
+++ src/components/about-me/socials/index.tsx
@@ -7,7 +7,7 @@ const Socials = () => {
         <SorcialLink href="https://github.com/masakifukunishi" service="GitHub" />
         <SorcialLink href="https://www.linkedin.com/in/masaki-fukunishi/" service="LinkedIn" />
         <SorcialLink href="https://dev.to/masakifukunishi" service="Dev Community (Articles)" />
-        <SorcialLink href="mailto:fmsknn@gmail.com " service="Email - fmsknn@gmail.com" />
+        <SorcialLink href="mailto:hello@masakifukunishi.com" service="Email - hello@masakifukunishi.com" />
         <h3 className="text-lg font-semibold mt-2">Japanese</h3>
         <SorcialLink href="https://twitter.com/masakifukunishi" service="X" />
         <SorcialLink href="https://zenn.dev/masakifukunishi" service="Zenn" />
